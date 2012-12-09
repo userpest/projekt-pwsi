019eb98091f9f70fb43ca7ebfac76ad9ab5b1634
uzyszkodnik <uzyszkodnik.reg@gmail.com>
2012-12-09 21:22:21 +0100

    created models

--- maps_site/maps_app/models.py
+++ maps_site/maps_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Address(models.Model):
+	owner = models.ForeignKey(User)
+	lat = models.FloatField('Latitude', blank = True, null=True)
+	lon = models.FloatField('Longitude', blank=True, null=True)
+	name = models.CharField(max_length=200)
+	comment = models.CharField(max_length=200)
+
+class SharedInfo(models.Model):
+	addr = models.ForeignKey(Address)
+	shared_user = models.ForeignKey(User)
