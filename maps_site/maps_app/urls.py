e4bede0f412a60f747dd834db5e3f399472b3027
uzyszkodnik <uzyszkodnik.reg@gmail.com>
2012-12-18 21:17:29 +0100

    fixd urls

--- maps_site/maps_app/urls.py
+++ maps_site/maps_app/urls.py
@@ -2,13 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('maps_app.views',
 
-                       url(r'^saved/$',
-                           'saved',
-			   name="maps_saved"),
-                       url(r'^shared/$',
-                           'shared',
-                           name='maps_shared'),
-                       url(r'^index/$',
+                       url(r'^$',
                            'index',
                            name='maps_index'),
 		     )
--- maps_site/maps_site/urls.py
+++ maps_site/maps_site/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^maps/', include('maps_app.urls')),
+     url(r'^$', include('maps_app.urls')),
      url(r'^accounts/', include('register.urls')),
      url(r'^admin/', include(admin.site.urls)),
      url(dajaxice_config.dajaxice_url, include('dajaxice.urls'))
