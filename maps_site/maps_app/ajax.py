8cee7b4369cfb468f525be2d0e29d4dbfcedbabd
uzyszkodnik <uzyszkodnik.reg@gmail.com>
2012-12-18 20:04:09 +0100

    finished location sharing

--- maps_site/maps_app/ajax.py
+++ maps_site/maps_app/ajax.py
@@ -72,7 +72,9 @@ def remove_saved_entry(request,e_id):
 	try:
 			addr =  Address.objects.get(id=e_id, owner=usr)
 			dajax.remove('#saved_entry_'+str(addr.id))			
+			SharedInfo.objects.filter(addr=addr).delete()
 			addr.delete()
+			
 
 	except Address.DoesNotExist:
 		return dajax.json()
@@ -131,3 +133,83 @@ def save_share_options(request, container,e_id,input_form):
 	html = render_to_string('maps_app/saved_locations.html',{'addrs': addrs})
 	dajax.append('#'+str(container), 'innerHTML', html)
 	return dajax.json()
+
+
+def saved_loc_show(request,container):
+	dajax = Dajax()
+	usr = get_user(request)
+	dajax.clear('#'+str(container), 'innerHTML')
+	addrs = Address.objects.filter(owner=usr)
+	html = render_to_string('maps_app/saved_locations.html',{'addrs': addrs})
+	dajax.append('#'+str(container), 'innerHTML', html)
+	return dajax.json()
+
+@dajaxice_register
+def show_saved_locations(request,container):
+	return saved_loc_show(request,container)
+
+def shared_loc_show(request,container):
+	dajax=Dajax()
+	dajax.clear('#'+str(container), 'innerHTML')
+
+	usr = get_user(request)
+	shared = SharedInfo.objects.filter(shared_user = usr)
+
+	addrs = [] 
+
+	for i in shared:
+		addrs.append(i.addr)
+
+	html = render_to_string('maps_app/shared_locations.html', {'addrs' : addrs } )
+	dajax.append('#'+str(container), 'innerHTML', html)
+	return dajax.json()
+
+
+@dajaxice_register
+def show_shared_locations(request,container):
+	return shared_loc_show(request,container)	
+
+@dajaxice_register
+def show_shared_location(request,e_id):
+	dajax=Dajax()
+	usr = get_user(request)
+
+	try:
+		addr = Address.objects.get(id=e_id)
+		shared = SharedInfo.objects.get(addr=addr, shared_user=usr)
+		points = { 'lat' : addr.lat, 'lng':addr.lng}
+		dajax.add_data(points,'set_location')
+
+	except SharedInfo.DoesNotExist:
+		pass
+	except Address.DoesNotExist:
+		pass
+
+	return dajax.json()
+
+@dajaxice_register
+def delete_shared_location(request,e_id):
+	dajax=Dajax()
+	usr = get_user(request)
+
+	try:
+		addr = Address.objects.get(id=e_id)
+
+		shared = SharedInfo.objects.get(addr=addr, shared_user=usr).delete()
+		dajax.remove('#shared_entry_'+str(addr.id))			
+
+	except SharedInfo.DoesNotExist:
+		pass
+	except Address.DoesNotExist:
+		pass
+	return dajax.json()
+
+
+@dajaxice_register
+def choose_showed_locations(request, option, container):
+
+	if int(option) == 0:
+		return saved_loc_show(request,container)
+	elif int(option) == 1: 
+		return shared_loc_show(request, container)
+
